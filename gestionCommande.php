5e8346f29c284eb42e0a20583dfc34a240c891da
Racem <jedidi.racem@gmail.com>
2017-01-30 00:37:30 +0100

    detailsClientCommande

--- gestionCommande.php
+++ gestionCommande.php
@@ -33,36 +33,58 @@ include("header.php");
                                 <thead>
                                 <tr>
                                     <th>Ref Commande</th>
+                                    <th>Client</th>
                                     <th>Date Commande</th>
                                     <th>Statut</th>
-                                    <th>Action</th>
+                                    <th>Produit(s)</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 <?php
                                 include_once("config/MyPDO.class.php");
                                 $connect = new MyPDO();
-                                $req="SELECT * FROM `commande` ORDER BY `dateCommande` DESC ";
+                                $req="SELECT * FROM `commande`,`utilisateur` WHERE `commande`.`utilisateur`=`utilisateur`.`idUtilisateur` ORDER BY `dateCommande` DESC ";
                                 $oPDOStatement=$connect->query($req);
                                 $oPDOStatement->setFetchMode(PDO::FETCH_OBJ);
                                 while ($row = $oPDOStatement->fetch()) {
                                     $refCommande = $row->idCommande;
-                                    $utilisaeur =$row->utilisateur;
+                                    $utilisaeur =$row->login;
                                     $dateCommandeE = $row->dateCommande;
                                     $traite =$row->traite;
                                     $dateCommande = date("d-m-Y", strtotime($dateCommandeE));
                                     ?>
                                     <tr>
-                                        <td>CMD<?php echo $refCommande; ?></td>
-                                        <td><?php echo $dateCommande; ?></td>
+                                        <td><a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe">CMD<?php echo $refCommande; ?></a></td>
+                                        <td><a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe"><?php echo $utilisaeur; ?></a></td>
+                                        <td><a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe"><?php echo $dateCommande; ?></a></td>
                                         <td>
-                                            <?php if ($traite==0){ echo '<span class="label bg-blue">non traité</span>';} ?>
-                                            <?php if ($traite==1){ echo '<span class="label bg-green">traité</span>';} ?>
+                                            <?php if ($traite==0){ ?>
+                                               <a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe">
+                                                <?php
+                                                echo '<span class="label bg-blue">non traité</span>';
+                                                echo '</a>';
+                                            } ?>
+                                            <?php if ($traite==1){ ?>
+                                                <a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe">
+                                               <?php
+                                                echo '<span class="label bg-green">traité</span>';
+                                                echo '</a>';
+                                            } ?>
                                         </td>
                                         <td>
-                                            <a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe">
-                                                <button type="button" class="btn btn-info waves-effect">détails</button>
-                                            </a>
+                                            <?php
+                                            $req2="SELECT `lignecommande`.`qte` AS qteCommande,`refProduit`,`etat` FROM `lignecommande`,`produits` WHERE `idCommande`='$refCommande'  AND `lignecommande`.`idProduit`=`produits`.`id` ";
+                                            $oPDOStatement2=$connect->query($req2);
+                                            $oPDOStatement2->setFetchMode(PDO::FETCH_OBJ);
+                                            $nbre=0;
+                                            while ($row2 = $oPDOStatement2->fetch()) {
+                                                $refProduit = $row2->refProduit;?>
+                                                <a href="detailsCommande.php?commande=<?php echo $refCommande; ?>&date=<?php echo $dateCommande; ?>&traite=<?php echo $traite; ?>" id="iframe">
+                                                <?php
+                                                echo $refProduit."<br>";
+                                                echo '</a>';
+                                            }
+                                            ?>
                                         </td>
                                     </tr>
                                     <?php
